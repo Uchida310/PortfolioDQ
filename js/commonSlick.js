008d8567c488d4672006bd3739d35626aaec26c4
Uchida310 <mitsuya.uchida310@gmail.com>
2022-01-10 14:02:15 +0900

    slick実装/画像追加

--- js/commonSlick.js
+++ js/commonSlick.js
@@ -1,4 +1,40 @@
-$('.slick').slick({
-    autoplay: true,
-    dots: true
+$('.slider').slick({
+    autoplay: true, //自動でスクロール
+    autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
+    speed: 5000, //スライドが流れる速度を設定
+    cssEase: "linear", //スライドの流れ方を等速に設定
+    slidesToShow: 5, //表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, //矢印非表示
+    pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 3, //画面幅750px以下でスライド3枚表示
+        }
+      }
+    ]
+});
+
+$('.slider02').slick({
+    autoplay: true, //自動でスクロール
+    autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
+    speed: 5000, //スライドが流れる速度を設定
+    cssEase: "linear", //スライドの流れ方を等速に設定
+    slidesToShow: 5, //表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, //矢印非表示
+    pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
+    rtl: true, //スライダーを左から右に流す（逆向き）
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 3, //画面幅750px以下でスライド3枚表示
+        }
+      }
+    ]
 });
